52bdbfed29fc87a20dc90ce9dea76478a26b1aa6
Luciano Junior <luciano@lucianojunior.com.br>
2016-06-29 22:08:07 -0300

    WooCommerce Payment Gateway Boilerplate

---
+++ wc-payment-gateway-boilerplate.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Plugin Name: WC Payment Gateway Boilerplate
+ * Description: A boilerplate for development payment gateways for WooCommerce.
+ * Version: 0.0.1
+ * Author: Luciano Junior
+ * Author URI: https://lucianojunior.com.br/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+?>
